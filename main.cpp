8517ffc8271f62a37a2645c49eed77b8ef2f3677
ian.smets <ian.smets@student.kuleuven.be>
2022-11-23 09:05:14 +0100

    working worldcreator

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "mainwindow.h"
+#include "world.h"
+#include <iostream>
 
 #include <QApplication>
 
@@ -6,6 +8,15 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+     QString init_worldmap = ":/images/world_images/maze2.png";
+
+     World test;
+     test.createWorld(init_worldmap, 5,5,0.25);
+     std::cout << "Columns:" << test.getCols() << "Rows:" << test.getRows() << std::endl;
+
     w.show();
     return a.exec();
+
+
 }
