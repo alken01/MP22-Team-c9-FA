ff05b231e87110fc83f9548f65e17ee464489450
ian.smets <ian.smets@student.kuleuven.be>
2022-12-04 11:55:57 +0100

    Textview map done

--- textview.cpp
+++ textview.cpp
@@ -1,21 +1,88 @@
 #include "textview.h"
 #include "world.h"
+#include <iostream>
 
 TextView::TextView()
 {}
 
 
-void initTextView(std::shared_ptr<QGraphicsScene> scene, std::shared_ptr<World> w){
-    //x = w->getCols();
-    //y = w->getRows();
+
+void TextView::initTextView(std::shared_ptr<QGraphicsScene> sc, std::shared_ptr<World> wo){
+    this->width = wo->getCols();
+    this->height = wo->getRows();
+    this->sc = sc;
+    this->qVec.clear();
+    this->world = wo;
+    this->enemies = wo->getEnemies();
+    this->healthPacks = wo->getHealthPacks();
+    this->protagonist = wo->getProtagonist();
 }
 
-void createMap(){
+void TextView::createMap(){
 
+    //Qstring = stores a string of 16-bit QChars --> implicit sharing: reduce memory usage and to avoid the needless copying of data
+    auto w = QString("+"); //Create Qstring for width
+    auto h = QString("|"); //Create Qstring for height
+
+    //for each column +---+ x width
+       for(int i = 0; i < width; i++){
+           w.append("---+");
+       }
+       //for each column |   | x width
+       for(int i = 0; i < width; i++){
+           h.append("   |");
+       }
+
+       //for each row repeat shape x height
+       for(int q = 0; q < height; q++){
+           qVec.push_back(w);
+           qVec.append("\n");
+           qVec.push_back(h);
+           qVec.append("\n");
+       }
+       //close map bottom line
+       qVec.push_back(w);
+
+       //couldn't make newline characters work when printing text seperately --> combine all first into single string
+       this->stringWorld = std::make_shared<QString>();
+       for (int i = 0; i < qVec.size(); ++i) {
+           stringWorld->append(qVec.at(i));
+       }
+
+       //add enemies
+       for (unsigned long i = 0; i <enemies.size(); ++i) {
+           unsigned long x = enemies.at(i)->getXPos();
+           unsigned long y = enemies.at(i)->getYPos();
+           changeSignAtCoord(x,y,'E');
+       }
+
+       //add health
+       for (unsigned long i = 0; i < healthPacks.size(); ++i) {
+           int x =healthPacks.at(i)->getXPos();
+           int y =healthPacks.at(i)->getYPos();
+           changeSignAtCoord(x,y,'H');
+       }
+
+       //add prot
+       unsigned long x= protagonist->getXPos();
+       unsigned long y = protagonist->getYPos();
+       changeSignAtCoord(x,y,'$');
+
+       this->sc->addText(*stringWorld,QFont("Monospace"));
 }
 
-void moveProtagonist(int x, int y, std::shared_ptr<Tile> &tile){
+void TextView::movProtagonist(){
+     unsigned long x= protagonist->getXPos();
+     unsigned long y = protagonist->getYPos();
+    changeSignAtCoord(x,y,'$');
+    sc->clear();
+    sc->addText(*stringWorld,QFont("Monospace"));
+}
 
+void TextView::changeSignAtCoord( unsigned long x,  unsigned long y, QChar input){
+    int rowlength = (width*4+2)*2;
+    int indexCount = rowlength*y-(width-x)*4-4;
+    this->stringWorld->replace(indexCount, 1,input);
 }
 
 
--- textview.h
+++ textview.h
@@ -8,15 +8,27 @@ class TextView : public View
 public:
     TextView();
 
+ void changeSignAtCoord( unsigned long x,  unsigned long y, QChar input);
 
- void initTextView(std::shared_ptr<QGraphicsScene> scene, std::shared_ptr<World> w);
+ void initTextView(std::shared_ptr<QGraphicsScene> sc, std::shared_ptr<World> w);
 
  void createMap();
 
- void moveProtagonist(int x, int y, std::shared_ptr<Tile> &tile);
+ void movProtagonist();
 
 private:
-     int x,y;
+ int width;
+ int height;
+ QVector<QString> qVec;
+ std::shared_ptr<QString> stringWorld;
+ std::shared_ptr<QGraphicsScene> sc;
+ std::shared_ptr<World> world;
+ std::vector<std::unique_ptr<Enemy>> enemies;
+ std::vector<std::unique_ptr<Tile>> healthPacks;
+ std::unique_ptr<Tile> protagonist;
+
+
+
 
  };
 
