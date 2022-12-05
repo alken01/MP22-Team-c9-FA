d9bb3c3ae8845820aa2028d4834f293d9a32d5bf
Fatjon Barçi <barcijoni@gmail.com>
2022-12-05 23:00:13 +0100

    More small changes to graphicalview

--- graphicalview.cpp
+++ graphicalview.cpp
@@ -3,8 +3,9 @@
 
 graphicalview::graphicalview(std::shared_ptr<World> world)
 {
-    this->scene = world->
+    this->height = world->getRows();
+    this->width = world->getCols();
     this->enemies = world->getEnemies();
     this->healthPacks = world->getHealthPacks();
-
+    this->protagonist = world->getProtagonist();
 }
