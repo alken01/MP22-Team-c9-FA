b5980146f5b682e3e11903e98dc2a32b7ff2c2fb
Alken Rrokaj <alken.rrokaj@student.kuleuven.be>
2022-12-07 03:11:17 +0100

    yessir

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,40 +1,74 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <QGraphicsScene>
-#include <QGraphicsRectItem>
-#include <QTimer>
-#include <iostream>
-#include <QtGui>
-#include "QtWidgets/qgraphicsscene.h"
-#include "QtWidgets/qgraphicsview.h"
-#include "mainwindow.h"
-#include "world.h"
 #include "controller.h"
 #include "graphicalview.h"
+#include "mainwindow.h"
+#include "QtWidgets/qgraphicsscene.h"
+#include "QtWidgets/qgraphicsview.h"
 #include "textview.h"
+#include "ui_mainwindow.h"
+#include "world.h"
+#include <iostream>
+#include <QApplication>
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QLineEdit>
+#include <QPainter>
+#include <QPixmap>
+#include <QPushButton>
+#include <Qt>
+#include <QtGui>
+#include <QTimer>
+#include <QVBoxLayout>
+
 
 MainWindow::MainWindow(QWidget *parent, QString init_worldmap )
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    auto test = std::make_shared<World>();
-    test->createWorld(init_worldmap, 5,5,0.25);
-    auto gview = std::make_shared<GraphicalView>();
-    auto tview = std::make_shared<TextView>();
-    Controller* controller = new Controller(test,gview,tview);
-    controller->switchToGraphic();
-//    QGraphicsScene* scene = new QGraphicsScene(this);
-    ui->graphicsView->setScene(controller->getScene());
-//    scene->addPixmap(QPixmap(init_worldmap));
-//    auto rect = scene->addRect(10, 50, 50, 120);
-//    rect->setZValue(1.1);
-//    rect->setFlag(QGraphicsItem::ItemIsMovable, true);
+    QGraphicsScene* scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+    scene->addPixmap(QPixmap(init_worldmap));
 
+    // Create a text input field
+    QLineEdit* textInput = new QLineEdit(this);
+
+    // Create a button
+    QPushButton* button = new QPushButton("Change View", this);
+
+    // Create a vertical layout to arrange the widgets
+    QVBoxLayout* layout = new QVBoxLayout;
+
+    // Add the graphicsView and the text input field to the layout
+    layout->addWidget(ui->graphicsView);
+    layout->addWidget(textInput);
+
+    // Set the layout for the main window
+    this->setLayout(layout);
+
+    // Add the button to the toolbar
+//    ui->mainToolBar->addWidget(button);
+
+    // Connect the button's clicked signal to the changeScene slot
+    connect(button, &QPushButton::clicked, this, &MainWindow::changeScene);
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::changeScene()
+{
+    // Create a new scene
+    QGraphicsScene* newScene = new QGraphicsScene(this);
+
+    // Add an item to the scene
+    newScene->addRect(QRectF(0, 0, 100, 100));
+
+    // Set the new scene for the graphicsView
+    ui->graphicsView->setScene(newScene);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr, QString init_worldmap = ":/images/world_images/maze1.png");
     ~MainWindow();
+    void changeScene();
+
 
 private:
     Ui::MainWindow *ui;
